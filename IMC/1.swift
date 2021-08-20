e8f6974e01bf921c1b371f07b4fba68d5082124a
Matheus Mendonca <matheus.mendonca@lavid.ufpb.br>
2021-08-20 15:59:24 -0300

    Primeiro commit

--- IMC/1.swift
+++ IMC/1.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tfPeso: UITextField!
+    
+    @IBOutlet weak var tfAltura: UITextField!
+    
+    @IBOutlet weak var lbResultado: UILabel!
+    
+    @IBOutlet weak var ivResultado: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calcular(_ sender: Any) {
+        
+    }
+    
 }
 
