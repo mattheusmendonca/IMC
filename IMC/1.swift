3c5eafc840d0cacd06f34cf89ee4c147ee7859f1
Thiago Andres <thiagopalacios@eng.ci.ufpb.br>
2021-08-24 01:30:01 -0300

    Prog sendo concluida

--- IMC/1.swift
+++ IMC/1.swift
@@ -10,12 +10,11 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var tfPeso: UITextField!
-    
     @IBOutlet weak var tfAltura: UITextField!
-    
     @IBOutlet weak var lbResultado: UILabel!
-    
     @IBOutlet weak var ivResultado: UIImageView!
+    
+    var imc: Double = 0;
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,7 +22,17 @@ class ViewController: UIViewController {
     }
 
     @IBAction func calcular(_ sender: Any) {
-        
+        //aqui ele ta criando uma variável do tipo let, e armazenando
+        //o texto extraido da altura e do peso, já convertido em double
+        //o "!", significa extração
+        if let peso = Double(tfPeso.text!), let altura = Double(tfAltura.text!){
+            imc = peso / (altura*altura);
+            showResults();
+        }
+    }
+    func showResults(){
+        var result: String = "";
+        var image: String = "";
     }
     
 }
